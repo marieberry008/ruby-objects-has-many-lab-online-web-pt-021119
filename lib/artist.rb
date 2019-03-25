4db1a9a913e9a2f45295af77744a3694ced8ea27
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-25 21:57:24 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  attr_reader :songs
 
   @@song_count = 0
 
@@ -21,9 +22,9 @@ class Artist
     @@song_count +=1
   end
 
-  def songs
-    @songs 
-  end
+  # def songs
+    # @songs 
+  # end
 
   def self.song_count
     @@song_count
