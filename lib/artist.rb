450673c84ac19aabddfa6dcaf8877e08a1b969c9
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-25 21:41:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
-  @@song_count = 0
+  # @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -12,14 +12,14 @@ class Artist
   def add_song(song)
     self.songs << song
     song.artist = self
-    @@song_count +=1
+    # @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
     song.artist = self
-    @@song_count +=1
+    # @@song_count +=1
   end
 
   # long form of attr_reader :songs
@@ -28,6 +28,7 @@ class Artist
   # end
 
   def self.song_count
-    @@song_count
+    @songs
+    # @@song_count
   end
 end
